8ea74952a53940ce54d25361448935c5b7cddd7c
Gary Cordero <GaryCordero1690@gmail.com>
2020-11-01 20:24:50 -0500

    articles new action  implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def index 
+    @articles = Article.all 
+  end
+
+  def new 
+
+  end
+
+  def create 
+    
+  end
+
 end
