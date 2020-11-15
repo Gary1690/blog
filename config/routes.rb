a7d3531d3e32ad9113e9595e3e1c807ba2cc817d
Gary Cordero <GaryCordero1690@gmail.com>
2020-11-15 13:16:37 -0500

    adding landing page and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "articles#index"
+  root "homes#landingPage"
+  get '/about', to: 'homes#about'
   # root :to => redirect('/articles')
   resources :articles
 
