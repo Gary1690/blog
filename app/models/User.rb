34dd041c62273eb0c28003215a9400f8a1260832
Gary Cordero <GaryCordero1690@gmail.com>
2020-11-19 16:31:35 -0500

    add one-tto-many association between users and articles

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  
+  has_many :articles
+
   validates :username, presence: true,
             uniqueness: {case_sensitive: false},
             length: {minimum:3, maximum:25}
@@ -8,4 +11,5 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false},
             length: {maximum:105},
             format: {with: VALID_EMAIL_REGEX}
+  
 end
