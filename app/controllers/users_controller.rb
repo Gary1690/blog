52137c8e5e2c6e055100ee29ae6f416533889bdf
Gary Cordero <GaryCordero1690@gmail.com>
2020-12-05 19:08:33 -0500

    finishing user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome, "+@user.username+ ". You have successfuly loggin."
+      flash[:notice] = "Welcome, #{@user.username}. You have successfuly loggin."
       redirect_to articles_path
     else
       render "new"
