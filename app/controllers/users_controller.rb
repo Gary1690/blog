f4d3fab05f663ae3df2b4704ae9332e0a058b191
Gary Cordero <GaryCordero1690@gmail.com>
2020-12-28 22:38:10 -0500

    authentication helpers added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit,:update]
 
   def index 
     @users = User.paginate(page:params[:page], per_page:5)
@@ -9,13 +10,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user =  User.find(params[:id])
     @articles = @user.articles.paginate(page:params[:page], per_page:5)
   end 
 
   def create 
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome, #{@user.username}. You have successfuly loggin."
       redirect_to articles_path
     else
@@ -24,11 +25,9 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user =  User.find(params[:id])
   end 
 
   def update
-    @user =  User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Yoour account information was successfully updated"
       redirect_to user_path(@user)
@@ -41,4 +40,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username,:email,:password)
   end
+
+  def set_user
+    @user =  User.find(params[:id])
+  end
+  
 end
