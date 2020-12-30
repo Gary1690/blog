cd2a576d9c93d8addce4dfdf51f27211fcffe4e2
Gary Cordero <GaryCordero1690@gmail.com>
2020-12-29 21:05:00 -0500

    applying controller actions constraints

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show,:edit,:update,:destroy]
+  before_action :require_user, except: [:index,:show]
+  before_action :require_same_user, only: [:update,:edit,:destroy]
 
   def show
     
@@ -52,4 +54,11 @@ class ArticlesController < ApplicationController
   def  set_article
     @article = Article.find(params[:id])
   end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit your own articles"
+      redirect_to articles_path
+    end
+  end 
 end
