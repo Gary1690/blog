7f6a0d313659dff296859043b5f6a54e6968130d
Gary Cordero <GaryCordero1690@gmail.com>
2021-01-22 22:02:53 -0500

    controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "homes#landingPage"
-  get '/about', to: 'homes#about'
-  # root :to => redirect('/articles')
-  resources :articles
-  get 'signup', to: 'users#new'
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
-
-  resources :users
+  resources :categories, except: [:destroy]
 end
