139171db8cd739be415663d93efa7a4db5603039
Gary Cordero <GaryCordero1690@gmail.com>
2021-01-23 17:37:46 -0500

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories, except: [:destroy]
+  root "homes#landingPage"
+  get '/about', to: 'homes#about'
+  # root :to => redirect('/articles')
+  resources :articles
+  get 'signup', to: 'users#new'
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  resources :users, except: [:new]
 end
